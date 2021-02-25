97d2b8984099d20f1b4d9a07ac6a89a89dc050cb
Juliusz Nadbereżny <jnd@touk.pl>
2021-02-25 14:08:15 +0100

    DB lookup cache + tests

--- nifi-ignite-extensions/nifi-ignite-services/src/main/java/pl/touk/nifi/services/DatabaseRecordLookupService.java
+++ nifi-ignite-extensions/nifi-ignite-services/src/main/java/pl/touk/nifi/services/DatabaseRecordLookupService.java
@@ -3,7 +3,6 @@ package pl.touk.nifi.services;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Expiry;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.annotation.lifecycle.OnEnabled;
@@ -17,17 +16,13 @@ import org.apache.nifi.lookup.RecordLookupService;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.serialization.record.Record;
 import org.apache.nifi.serialization.record.ResultSetRecordSet;
-import org.apache.nifi.util.Tuple;
-import pl.touk.nifi.common.ThrowingConsumer;
+import pl.touk.nifi.services.lookup.sql.LookupSqlArgument;
+import pl.touk.nifi.services.lookup.sql.LookupSqlQuery;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -37,11 +32,30 @@ import java.util.stream.Stream;
         + "will be returned for each lookup, duplicate database entries are ignored.")
 public class DatabaseRecordLookupService extends AbstractDatabaseLookupService implements RecordLookupService {
 
-    private volatile Cache<Tuple<String, Object>, Record> cache;
+    private volatile Cache<LookupSqlQuery, Record> cache;
 
     protected static final String ARG_PREFIX = "arg";
 
     protected static final String ARGS_PATTERN = "^arg[0-9]+$";
+
+    static final Set<String> REQUIRED_KEYS = Collections.emptySet();
+
+    static final PropertyDescriptor DBCP_SERVICE = new PropertyDescriptor.Builder()
+            .name("dbrecord-lookup-dbcp-service")
+            .displayName("Database Connection Pooling Service")
+            .description("The Controller Service that is used to obtain connection to database")
+            .required(true)
+            .identifiesControllerService(DBCPService.class)
+            .build();
+
+    static final PropertyDescriptor TABLE_NAME = new PropertyDescriptor.Builder()
+            .name("dbrecord-lookup-table-name")
+            .displayName("Table Name")
+            .description("The name of the database table to be queried. Note that this may be case-sensitive depending on the database.")
+            .required(true)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .build();
 
     static final PropertyDescriptor LOOKUP_VALUE_COLUMNS = new PropertyDescriptor.Builder()
             .name("dbrecord-lookup-value-columns")
@@ -61,42 +75,49 @@ public class DatabaseRecordLookupService extends AbstractDatabaseLookupService i
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .build();
 
+    protected List<PropertyDescriptor> properties;
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return properties;
+    }
+
     @Override
     protected void init(final ControllerServiceInitializationContext context) {
         final List<PropertyDescriptor> properties = new ArrayList<>();
         properties.add(DBCP_SERVICE);
         properties.add(TABLE_NAME);
         properties.add(LOOKUP_VALUE_COLUMNS);
+        properties.add(WHERE_CLAUSE);
         properties.add(CACHE_SIZE);
         properties.add(CLEAR_CACHE_ON_ENABLED);
         properties.add(CACHE_EXPIRATION);
-        properties.add(WHERE_CLAUSE);
         this.properties = Collections.unmodifiableList(properties);
     }
 
     @OnEnabled
     public void onEnabled(final ConfigurationContext context) {
-        this.dbcpService = context.getProperty(DBCP_SERVICE).asControllerService(DBCPService.class);
-        final int cacheSize = context.getProperty(CACHE_SIZE).evaluateAttributeExpressions().asInteger();
-        final boolean clearCache = context.getProperty(CLEAR_CACHE_ON_ENABLED).asBoolean();
-        final long durationNanos = context.getProperty(CACHE_EXPIRATION).isSet() ? context.getProperty(CACHE_EXPIRATION).evaluateAttributeExpressions().asTimePeriod(TimeUnit.NANOSECONDS) : 0L;
+        dbcpService = context.getProperty(DBCP_SERVICE).asControllerService(DBCPService.class);
+        int cacheSize = context.getProperty(CACHE_SIZE).evaluateAttributeExpressions().asInteger();
+        boolean clearCache = context.getProperty(CLEAR_CACHE_ON_ENABLED).asBoolean();
+        long durationNanos = context.getProperty(CACHE_EXPIRATION).isSet() ? context.getProperty(CACHE_EXPIRATION).evaluateAttributeExpressions().asTimePeriod(TimeUnit.NANOSECONDS) : 0L;
         if (this.cache == null || (cacheSize > 0 && clearCache)) {
             if (durationNanos > 0) {
                 this.cache = Caffeine.newBuilder()
                         .maximumSize(cacheSize)
-                        .expireAfter(new Expiry<Tuple<String, Object>, Record>() {
+                        .expireAfter(new Expiry<LookupSqlQuery, Record>() {
                             @Override
-                            public long expireAfterCreate(Tuple<String, Object> stringObjectTuple, Record record, long currentTime) {
+                            public long expireAfterCreate(LookupSqlQuery query, Record record, long currentTime) {
                                 return durationNanos;
                             }
 
                             @Override
-                            public long expireAfterUpdate(Tuple<String, Object> stringObjectTuple, Record record, long currentTime, long currentDuration) {
+                            public long expireAfterUpdate(LookupSqlQuery query, Record record, long currentTime, long currentDuration) {
                                 return currentDuration;
                             }
 
                             @Override
-                            public long expireAfterRead(Tuple<String, Object> stringObjectTuple, Record record, long currentTime, long currentDuration) {
+                            public long expireAfterRead(LookupSqlQuery query, Record record, long currentTime, long currentDuration) {
                                 return currentDuration;
                             }
                         })
@@ -107,6 +128,7 @@ public class DatabaseRecordLookupService extends AbstractDatabaseLookupService i
                         .build();
             }
         }
+
     }
 
     @Override
@@ -120,16 +142,9 @@ public class DatabaseRecordLookupService extends AbstractDatabaseLookupService i
             return Optional.empty();
         }
 
-        // TODO
-        final Object key = coordinates.get("TODO");
-        if (StringUtils.isBlank(key.toString())) {
-            return Optional.empty();
-        }
-
-        final String tableName = getProperty(TABLE_NAME).evaluateAttributeExpressions(context).getValue();
-        final String lookupValueColumnsList = getProperty(LOOKUP_VALUE_COLUMNS).evaluateAttributeExpressions(context).getValue();
-        final String whereClause = getProperty(WHERE_CLAUSE).evaluateAttributeExpressions(context).getValue();
-
+        String tableName = getProperty(TABLE_NAME).evaluateAttributeExpressions(context).getValue();
+        String lookupValueColumnsList = getProperty(LOOKUP_VALUE_COLUMNS).evaluateAttributeExpressions(context).getValue();
+        String whereClause = getProperty(WHERE_CLAUSE).evaluateAttributeExpressions(context).getValue();
         Set<String> lookupValueColumnsSet = new LinkedHashSet<>();
         if (lookupValueColumnsList != null) {
             Stream.of(lookupValueColumnsList)
@@ -138,72 +153,46 @@ public class DatabaseRecordLookupService extends AbstractDatabaseLookupService i
                     .map(String::trim)
                     .forEach(lookupValueColumnsSet::add);
         }
+        LookupSqlQuery query = new LookupSqlQuery(lookupValueColumnsSet, tableName, whereClause, getQueryArgs(coordinates));
 
-        final String lookupValueColumns = lookupValueColumnsSet.isEmpty() ? "*" : String.join(",", lookupValueColumnsSet);
-
-        Tuple<String, Object> cacheLookupKey = new Tuple<>(tableName, key);
-
-        // Not using the function param of cache.get so we can catch and handle the checked exceptions
-        Record foundRecord = cache.get(cacheLookupKey, k -> null);
-
-        if (foundRecord == null) {
-            final String selectQuery = "SELECT " + lookupValueColumns + " FROM " + tableName + " WHERE " + whereClause;
-
-            try (final Connection con = dbcpService.getConnection(context);
-                 final PreparedStatement st = con.prepareStatement(selectQuery)) {
-
-                getQueryArgs(coordinates).forEach(
-                        consumeWithSqlExceptionHandle(arg ->
-                                st.setObject(arg.getKey(), arg.getValue())
-                        )
-                );
-                ResultSet resultSet = st.executeQuery();
+        Record recordFromCache = cache.get(query, k -> null);
+        if (recordFromCache == null) {
+            Connection conn = dbcpService.getConnection(context);
+            try {
+                PreparedStatement statement = query.toStatement(conn);
+                ResultSet resultSet = statement.executeQuery();
                 ResultSetRecordSet resultSetRecordSet = new ResultSetRecordSet(resultSet, null);
-                foundRecord = resultSetRecordSet.next();
-
-                // Populate the cache if the record is present
-                if (foundRecord != null) {
-                    cache.put(cacheLookupKey, foundRecord);
+                Record record = resultSetRecordSet.next();
+                if (record != null) {
+                    cache.put(query, record);
                 }
-
+                return Optional.ofNullable(record);
             } catch (SQLException se) {
-                throw new LookupFailureException("Error executing SQL statement: " + selectQuery + "for value " + key.toString()
-                        + " : " + (se.getCause() == null ? se.getMessage() : se.getCause().getMessage()), se);
+                throw new LookupFailureException("Error executing SQL statement. " + (se.getCause() == null ? se.getMessage() : se.getCause().getMessage()), se);
             } catch (IOException ioe) {
-                throw new LookupFailureException("Error retrieving result set for SQL statement: " + selectQuery + "for value " + key.toString()
-                        + " : " + (ioe.getCause() == null ? ioe.getMessage() : ioe.getCause().getMessage()), ioe);
+                throw new LookupFailureException("Error retrieving result set for SQL statement. " + (ioe.getCause() == null ? ioe.getMessage() : ioe.getCause().getMessage()), ioe);
             }
+        } else {
+            return Optional.of(recordFromCache);
         }
-
-        return Optional.ofNullable(foundRecord);
     }
 
     private static boolean isNotBlank(final String value) {
         return value != null && !value.trim().isEmpty();
     }
 
-    private static List<Tuple<Integer, Object>> getQueryArgs(Map<String, Object> coordinates) {
+    private Set<LookupSqlArgument> getQueryArgs(Map<String, Object> coordinates) {
         return coordinates.entrySet().stream()
                 .filter(entry -> entry.getKey().matches(ARGS_PATTERN))
                 .map(entry -> {
-                    Integer argIx = Integer.valueOf((entry.getKey()).replaceFirst(ARG_PREFIX, ""));
-                    return new Tuple<Integer, Object>(argIx, entry.getValue());
+                    int argIx = Integer.parseInt((entry.getKey()).replaceFirst(ARG_PREFIX, ""));
+                    return new LookupSqlArgument(argIx, entry.getValue());
                 })
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
     }
 
     @Override
     public Set<String> getRequiredKeys() {
         return REQUIRED_KEYS;
-    }
-
-    static <A> Consumer<A> consumeWithSqlExceptionHandle(ThrowingConsumer<A, SQLException> consumer) {
-        return a -> {
-            try {
-                consumer.accept(a);
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        };
     }
 }
